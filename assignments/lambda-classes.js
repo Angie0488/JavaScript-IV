7c61d49f98856b969983d7dcdc404d22c12bdea2
Angie0488 <43270346+Angie0488@users.noreply.github.com>
2019-08-15 22:01:50 -0400

    completed stretch tasks

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -22,7 +22,10 @@ class Instructor extends Person {
         return `Today we are learning about ${subject}.`;
     }
     grade(subject) {
-        return `${student.name} receives a perfect score on ${subject}.`
+        return `${joseph.name} receives a perfect score on ${subject}.`
+    }
+    randomGrade() {
+        return Math.random() * Math.ceil(danielle.grade);
     }
 }
 
@@ -32,15 +35,23 @@ class Student extends Person {
         this.previousBackground = studentAttrs.previousBackground;
         this.className = studentAttrs.className;
         this.favSubjects = studentAttrs.favSubjects;
+        this.grade = studentAttrs.grade;
     }
     listsSubjects() {
         return `${this.favSubjects}`;
     }
     PRAssignment(subject) {
-        return `${student.name} has submitted a PR for ${subject}.`;
+        return `${danielle.name} has submitted a PR for ${subject}.`;
     }
     sprintChallenge(subject) {
-        return `${student.name} has begun the sprint challenge on ${subject}.`
+        return `${danielle.name} has begun the sprint challenge on ${subject}.`
+    }
+    graduate() {
+        if(joseph.grade >= 70) {
+            return `Let them Graduate!`
+        } else {
+            return `Needs to complete summer school!`
+        }
     }
 }
 
@@ -54,7 +65,7 @@ class TeamLead extends Instructor {
         return `${this.name} announces to ${channel}, @channel standy times!`;
     }
     debugsCode(student, subject) {
-        return `${this.name} debugs ${student.name}'s code on ${subject}.`
+        return `${this.name} debugs ${joseph.name}'s code on ${subject}.`
     }
 }
 
@@ -67,13 +78,34 @@ const instructor = new Instructor ({
     catchPhrase: 'Balls!'
 });
 
-const student = new Student ({
+const joseph = new Student ({
     name: 'Joseph',
     location: 'Merced',
     age: 27,
     previousBackground: 'Chef',
     className: 'Webpt9',
-    favSubjects: ['HTML', 'CSS', 'JavaScript']
+    favSubjects: ['HTML', 'CSS', 'JavaScript'],
+    grade: 90
+});
+
+const danielle = new Student ({
+    name: 'Danielle',
+    location: 'Merced',
+    age: 18,
+    previousBackground: 'Cashier',
+    className: 'Webpt8',
+    favSubjects: ['HTML', 'CSS', 'JavaScript'],
+    grade: 85
+});
+
+const melanie = new Student ({
+    name: 'Melanie',
+    location: 'Merced',
+    age: 20,
+    previousBackground: 'Bank Teller',
+    className: 'Web20',
+    favSubjects: ['HTML', 'CSS', 'JavaScript'],
+    grade: 65
 });
 
 const teamLead = new TeamLead ({
@@ -86,9 +118,10 @@ const teamLead = new TeamLead ({
 
 console.log(instructor.demo('Web Dev'));
 console.log(instructor.grade('JavaScript I'));
-console.log(student.listsSubjects());
-console.log(student.PRAssignment('JavaScript II'));
-console.log(student.sprintChallenge('JavaScript II'));
+console.log(joseph.listsSubjects());
+console.log(melanie.PRAssignment('JavaScript II'));
+console.log(danielle.sprintChallenge('JavaScript II'));
 console.log(teamLead.standUp('#Webpt9-David'));
 console.log(instructor.grade('JavaScript I'));
-
+console.log(instructor.randomGrade());
+console.log(joseph.graduate());
